b5a690cbd47b91e52a19051d2d187305b6071b60
Maurice <106934566+MauriceTZ@users.noreply.github.com>
2024-02-26 18:56:29 -0300

    Update SMTool.py
    Docstring'd comments

--- SMTool.py
+++ SMTool.py
@@ -73,13 +73,15 @@ class Blueprint(dict):
 
 
     def get_path():
-        #Iterates through %APPDATA% and returns a list of all the directories behind the given path
-        #Then get's the [1] item of that list, which should be the blueprint folder.
+        """Iterates through %APPDATA% and returns a list of all the directories behind the given path
+        
+        Then get's the [1] item of that list, which should be the blueprint folder."""
         return [x[0] for x in os.walk(os.getenv('APPDATA')+"\\Axolot Games\\Scrap Mechanic\\User")][1]
 
 
-    #Uses a search term to retrieve the blueprint file of a creation ! WITH A SPECIFIC NAME.lower() !
+    
     def blueprint_search(self,search_term):
+        """Uses a search term to retrieve the blueprint file of a creation ! WITH A SPECIFIC NAME.lower() !"""
         blueprints = [x[0] for x in os.walk(self.get_path()+"\\Blueprints")]
         for i in range(len(blueprints)):
             if i==0:
